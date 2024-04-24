dac8097d533b3f480c815f6840db7b9914afcb11
Mrityunjay Kumar <kumarmrityunjay7034@gmail.com>
2024-04-24 12:20:49 +0530

    reporting bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
-import './App.css'
-import { Button, Divider, Flex, Input, Modal, Typography, notification } from 'antd'
+import "./App.css";
+import { Button, Flex, Input, Modal, Radio, Typography, notification } from 'antd'
 import axios from 'axios';
 
 const { Text, Title } = Typography;
 
 function App() {
 
-  const [callId, setCallId] = useState(location?.search?.replace("?", "").split("=")[1]);
+  const paramsRaw = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(paramsRaw.entries());
 
   const [fields, setFields]: any = useState({ firstname: "", lastname: "", email: "", feedback: "", rating: "" });
 
@@ -19,24 +20,15 @@ function App() {
 
   const [lastFocusField, setLastFocusField] = useState("");
 
+  const [q1, setQ1] = useState("");
+  const [q2, setQ2] = useState("");
+  const [q3, setQ3] = useState("");
+  const [q4, setQ4] = useState("");
+  const [q5, setQ5] = useState("");
+  const [q6, setQ6] = useState("");
+
   const callAPIOnMouseEnter = async (fieldName: string) => {
     try {
-
-      notification.success({
-        message: `API called for field : ${fieldName}`
-      });
-
-      const res = await axios({
-        method: "POST",
-        url: apiUrl,
-        data: {
-          type: "fieldwise",
-          fieldName,
-          callId
-        }
-      });
-
-      console.log(`response of api call : ${apiUrl} is : `, res);
 
     } catch (error) {
 
@@ -46,19 +38,7 @@ function App() {
   const sendAllData = async () => {
     try {
 
-      const res = await axios({
-        method: "POST",
-        url: apiUrl,
-        data: {
-          type: "allFields",
-          fields,
-          callId
-        }
-      });
 
-      if (res?.data) {
-        console.log("response for sending all data : ", res?.data);
-      }
 
     } catch (error) {
 
@@ -104,109 +84,145 @@ function App() {
 
       </Modal>
 
-      <Flex vertical style={{ padding: 20, position: "fixed", top: 0, bottom: 0, left: "20%", right: "20%" }}>
+      <Flex vertical className='form-container'>
 
+        <Flex style={{ zIndex: -1, position: "absolute", top: 0, left: 0, bottom: 0, right: 0, background: 'url("/lg_logo.png") no-repeat center', backgroundSize: "60%" }}><Text></Text></Flex>
 
         <Flex justify="space-between">
-          <Title level={2}>API Details</Title>
-          <Button onClick={() => {
-            if (!apiUrl) notification.error({ message: "API yrl to dalo" });
-            localStorage && localStorage.setItem("au", apiUrl);
-          }} style={{ width: "20%" }} type='primary'>Save</Button>
+          <Title level={3}>Welcome to LG Visual IVR</Title>
         </Flex>
 
-        <Flex vertical style={{}}>
+        <Flex>
+          <Title style={{ color: "grey" }} level={5}>Please share your valuable feedback regarding recent repair of your LG {params?.productName ?? "NA"}</Title>
+        </Flex>
 
-          <Flex vertical style={{ marginBottom: 10 }}>
-            <Text>API URL</Text>
-            <Input
-              value={apiUrl}
-              placeholder='API URL'
-              onChange={(e) => {
-                setAPIUrl(e.target.value)
-              }}
-            />
+        {/* Question 1 */}
+        <Flex>
 
-            <br />
+          <Text style={{ flex: 0.06 }}>1</Text>
+
+          <Flex vertical style={{ flex: 0.94 }}>
+            <Title level={5}>Your product has been repaired against request number {params?.reqNo ?? "NA"}</Title>
+
+            <Radio.Group onChange={(e: any) => { setQ1(e.target.value) }} value={q1}>
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+            </Radio.Group>
 
           </Flex>
 
         </Flex>
 
-        <Divider></Divider>
+        {/* Question 2 */}
+        <Flex>
 
-        <Flex justify="space-between">
-          <Title level={2}> Fields </Title>
-          <Button style={{ width: "20%" }} type='primary' onClick={() => setShowAddFieldModal(true)}>Add Field + </Button>
+          <Text style={{ flex: 0.06 }}>2</Text>
+
+          <Flex vertical style={{ flex: 0.94 }}>
+            <Title level={5}>Did engineer change any part during repair of the product?</Title>
+            <Radio.Group onChange={(e: any) => { setQ2(e.target.value) }} value={q2}>
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+              <Radio value={"notSure"}>Not Sure</Radio>
+            </Radio.Group>
+          </Flex>
+
         </Flex>
 
+        {/* Question 3 */}
+        <Flex>
 
-        <Flex vertical style={{}}>
+          <Text style={{ flex: 0.06 }}>3</Text>
 
-          <Flex vertical style={{ marginBottom: 10 }}>
-            <Text>Call Id</Text>
-            <Input
-              value={callId}
-              placeholder='Call Id'
-              onChange={(e) => setCallId(e.target.value)}
-            />
+          <Flex vertical style={{ flex: 0.94 }}>
 
-            <br />
+            <Title level={5}>Did you pay any charges for repair?</Title>
+            <Radio.Group onChange={(e: any) => { setQ3(e.target.value) }} value={q3}>
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+              <Radio value={"notSure"}>Not Sure</Radio>
+            </Radio.Group>
 
           </Flex>
 
         </Flex>
 
-        <Flex style={{}} vertical>
+        {/* Question 4 */}
+        <Flex>
+          <Text style={{ flex: 0.06 }}>4</Text>
 
-          {
-            Object.keys(fields)?.map((field: any) => {
-              return (
-                <>
+          <Flex vertical style={{ flex: 0.94 }}>
 
-                  <Flex vertical style={{ marginBottom: 10 }}>
-                    <Text>{field}</Text>
-                    <Input
-                      value={fields[field]}
-                      placeholder={field}
-                      onFocus={() => {
-                        if (lastFocusField !== field) {
-                          callAPIOnMouseEnter(field);
-                          setLastFocusField(field);
-                        }
-                      }}
-                      onChange={(e) => {
-                        setFields((prev: any) => {
-                          return { ...prev, [field]: e.target.value }
-                        })
-                      }}
-                    />
-                  </Flex>
+            <Title level={5}>Did you pay any charges for purchase of AMC / D-Scale or any other Accessory during Repair?</Title>
 
-                </>
-              );
-            })
-          }
+            <Radio.Group onChange={(e: any) => { setQ4(e.target.value) }} value={q4}>
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+              <Radio value={"notSure"}>Not Sure</Radio>
+            </Radio.Group>
+
+          </Flex>
 
         </Flex>
 
-        <br />
+        {/* Question 5 */}
+        <Flex>
+
+          <Text style={{ flex: 0.06 }}>5</Text>
+
+          <Flex vertical style={{ flex: 0.94 }}>
+
+            <Title level={5}>Did you get Bill / Invoice of payment ?</Title>
+
+            <Radio.Group onChange={(e: any) => { setQ5(e.target.value) }} value={q5}>
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+            </Radio.Group>
+
+          </Flex>
+
+        </Flex>
+
+        {/* Question 6 */}
+        <Flex>
+
+          <Text style={{ flex: 0.06 }}>6</Text>
+
+          <Flex vertical style={{ flex: 0.94 }}>
+
+            <Title level={5}>Please Confirm the type of Bill / Invoice Engineer provided to you</Title>
+
+            <Radio.Group onChange={(e: any) => { setQ6(e.target.value) }} value={q6}>
+              <Radio value={"digital"}>Digital</Radio>
+              <Radio value={"handWritten"}>Hand Written</Radio>
+            </Radio.Group>
+
+          </Flex>
+
+        </Flex>
+
+        <Flex>
+          <Text style={{ flex: 0.06 }}>7</Text>
+          <Flex vertical style={{ flex: 0.94 }}>
+            <Title level={5}>Remarks</Title>
+            <Input.TextArea
+              placeholder='Please enter remarks'
+              rows={3}>
+            </Input.TextArea>
+          </Flex>
+        </Flex>
+
 
         <Flex justify='flex-end' gap={20}>
 
-          <Button onClick={() => {
-            setFields({ firstname: "", lastname: "", email: "", feedback: "", rating: "" });
-            setCallId("");
-          }} style={{}}>Clear</Button>
-
-          <Button onClick={() => {
+          <Button size="large" onClick={() => {
             sendAllData();
             setLastFocusField("");
-          }} style={{ background: "orange" }}>Submit</Button>
+          }} style={{ padding: "0 3rem", background: "rgba(151, 29, 55, 1)", color: "white" }}>Submit</Button>
 
         </Flex>
 
-      </Flex >
+      </Flex>
 
     </>
   )
